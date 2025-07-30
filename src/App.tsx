f02f7099565e2b20682bed1b5bf3f7cf2f21c165
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-07-29 22:02:11 -0300

    feat:initializing home page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-export function App(){
+export function App() {
   return (
-    <div>
-      
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-4xl w-full px-8 text-center space-y-10">
+        <p className="text-zinc-300 text-lg">
+          Convide seus amigos e planeje sua próxima viagem!
+        </p>
+        <form action="" className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center" >
+          <input type="text" placeholder="Para onde você vai?" />
+          <input type="text" placeholder="Quando?" />
+          <button>Continuar</button>
+        </form>
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
+          <br /> com nossos{" "}
+          <a href="" className="text-zinc-300 underline">
+            termos de uso
+          </a>{" "}
+          e{" "}
+          <a href="" className="text-zinc-300 underline">
+            políticas de privacidade
+          </a>
+          .
+        </p>
+      </div>
     </div>
-  )
+  );
 }
