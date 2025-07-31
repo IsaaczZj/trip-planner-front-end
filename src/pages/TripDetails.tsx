d8b38d61c3a96c4c0d3944376964051ecab3f7c4
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-07-31 17:44:23 -0300

    feat:trip details page

--- src/pages/TripDetails.tsx
+++ src/pages/TripDetails.tsx
@@ -1,4 +1,13 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
+import {
+  Calendar,
+  CircleCheck,
+  CircleDashed,
+  Link2,
+  MapPin,
+  Plus,
+  Settings2,
+  UserCog,
+} from "lucide-react";
 
 export function TripDetails() {
   return (
@@ -66,7 +75,55 @@ export function TripDetails() {
             </div>
           </div>
         </div>
-        <div className="w-80"></div>
+        <div className="w-80 space-y-6">
+          <div className="space-y-6 ">
+            <h3 className="font-semibold text-xl">Links importantes</h3>
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5 max-w-[220px]">
+                  <span className="text-zinc-100 font-medium block">
+                    Reserva do AirBnB
+                  </span>
+                  <a
+                    href="#"
+                    className="text-zinc-400 block text-sm truncate hover:text-zinc-200"
+                  >
+                    https://www.airbnb.com.br/rooms/104700011
+                  </a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0" />
+              </div>
+            </div>
+
+            <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:opacity-75 cursor-pointer w-full">
+              <Plus className="size-5" />
+              Cadastrar novo link
+            </button>
+          </div>
+          <div className="w-full h-px bg-zinc-800 my-6"></div>
+
+          <div className="space-y-6 ">
+            <h3 className="font-semibold text-xl">Convidados</h3>
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5 max-w-[220px]">
+                  <span className="text-zinc-100 font-medium block">
+                    Jessica White
+                  </span>
+                  <span className="text-zinc-400 block text-sm truncate ">
+                    jessica.white44@yahoo.com
+                  </span>
+                </div>
+                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              </div>
+            </div>
+
+            <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:opacity-75 cursor-pointer w-full">
+              <UserCog className="size-5" />
+              Gerenciar convidados
+            </button>
+          </div>
+        </div>
       </main>
     </div>
   );
